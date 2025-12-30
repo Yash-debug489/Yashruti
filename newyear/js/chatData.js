b88c6c2007e12ddbd2844969055687a9615ea2b8
Yash-debug489 <yash.kumaranime@gmail.com>
2025-12-30 14:34:17 +0530

    Create chatData.js

---
+++ newyear/js/chatData.js
@@ -0,0 +1,28 @@
+const CHAT = [
+  // PAGE 1
+  { side: "left",  text: "Yooo..so ur fianlly heree...welcome make urself comfortable.." },
+  { side: "right", text: "Yupp..waisee haii kyaa yahann pee.." },
+  { side: "left",  text: "sabar karoo madam\nu know slowly opening up..build up before climax" },
+  { side: "left", text: "waise u know why i'm so happy??" },
+  { side: "right",  text: "kyuu??" },
+  { side: "left", text: "Im entering 2026 with the best thing ever😌" },
+  { side: "right",  text: "Assa kyaa haii?" },
+  { side: "left", text: "👉👈" },
+  { side: "left",  text: "Wanna see it??" },
+  { side: "left", text: `click <a href="camera.html">HERE</a>`, html: true },
+
+  // PAGE 2
+  { side: "right", text: "😳💕cutee thaa yrr" },
+  { side: "left",  text: "wait, website khtm nii huii haii" },
+  { side: "right", text: "aggee bhii kuch haii??" },
+  { side: "left",  text: "haa meri profile pic pe click karo👉👈" },
+
+  // PAGE 3
+  { side: "left",  text: "hehe, its me again......\nWaise i wanted to tell u kii i know i can't feel period cramps myself...but i hate it that u have to go through it every monthh..\nSO i ordered u EVA PERIOD HEATING PAD i hope it makes u feel a little more comfortable❤️\nI know abhii unclee haii😭, i didn't knew jab order kiyaa thaa..it will get delivered aj 1 jan ko hiii, i will get it placed at the usual spot fir text ya call krdunga...you can take it whenever you feel comfortable" },
+  { side: "left",  text: "okayy..waise password for post2 is - us4life" },
+
+  // PAGE 4
+  { side: "left",  text: "The website is over noww, but we're just beginning" },
+  { side: "left",  text: "Happy new year, my love❤️ and Happy new year to us!!" }
+];
+
